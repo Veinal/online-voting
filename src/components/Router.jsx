2849e06cb132ae489b6b85579899ce4ea90d7b97
VEINAL SEQUEIRA <veinalsequeira5229@gmail.com>
2024-08-07 23:46:26 +0530

    ninth commit

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import NavBar from './NavBar'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 
 export default function Router() {
+
+  const WithNavBar = ({ children }) => (
+    <>
+      <NavBar />
+      {children}
+    </>
+  );
+
   return (
     <div>
         <BrowserRouter>
-            <NavBar/>
-            
             <Routes>
-                <Route exact path='/' element={<Home/>}/>
+                <Route path='/' element={<WithNavBar><Home/></WithNavBar>}/>
+                <Route path='/signin' element={<SignIn/>}/>
+                <Route path='/signup' element={<SignUp/>}/>
             </Routes>
         </BrowserRouter>
     </div>
