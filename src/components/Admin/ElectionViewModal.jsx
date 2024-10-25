78dfcac3596ee1213c80a85831a840e59bbda6d4
VEINAL SEQUEIRA <veinalsequeira5229@gmail.com>
2024-10-25 18:58:13 +0530

    24th commit

--- src/components/Admin/ElectionViewModal.jsx
+++ src/components/Admin/ElectionViewModal.jsx
@@ -27,6 +27,15 @@ export default function ElectionViewModal(props) {
           <p className="text-gray-700 mb-4">
             <strong className="text-lg">End Date:</strong> <span className="text-gray-600">{new Date(props.selectedElect.endDate).toLocaleDateString()}</span>
           </p>
+          <p className="text-gray-700 mb-4">
+          <strong className="text-lg">Candidates:</strong>
+            <span className="text-gray-600">
+                {props.selectedElect?.candidate_id.map((candidate) => {
+                    // Access the candidate's userName from the user_id object
+                    return candidate?.user_id?.userName || 'Unknown';
+                }).join(', ')} {/* Join the names with commas if multiple candidates */}
+            </span>
+          </p>
         </div>
 
         <div className="flex justify-center">
